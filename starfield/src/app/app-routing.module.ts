8f7a655421a10915d298668682fe950b1a4e52a1
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2023-08-29 17:21:19 +0200

    third commit (technique routes)

--- starfield/src/app/app-routing.module.ts
+++ starfield/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { PreReleasePageComponent } from './pages/pre-release-page/pre-release-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/main-page', pathMatch:'full'},
+  {path: "pre-release-page", component: PreReleasePageComponent},
+  {path: "main-page", component: MainPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- starfield/src/app/app.module.ts
+++ starfield/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './component/main-page/main-page.component';
-import { ModalPageComponent } from './component/modal-page/modal-page.component';
-import { StarPageComponent } from './component/star-page/star-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ModalPageComponent } from './pages/modal-page/modal-page.component';
+import { StarPageComponent } from './pages/star-page/star-page.component';
+import { PreReleasePageComponent } from './pages/pre-release-page/pre-release-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     ModalPageComponent,
-    StarPageComponent
+    StarPageComponent,
+    PreReleasePageComponent
   ],
   imports: [
     BrowserModule,
