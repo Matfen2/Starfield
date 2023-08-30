b79a8a5c40d0fd50757d3c75cb32b49ed8667e98
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2023-08-30 18:00:18 +0200

    four commit

--- starfield/src/app/app.module.ts
+++ starfield/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { ModalPageComponent } from './pages/modal-page/modal-page.component';
-import { StarPageComponent } from './pages/star-page/star-page.component';
 import { PreReleasePageComponent } from './pages/pre-release-page/pre-release-page.component';
+import { JoinPageComponent } from './pages/join-page/join-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { InscribePageComponent } from './pages/inscribe-page/inscribe-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     ModalPageComponent,
-    StarPageComponent,
-    PreReleasePageComponent
+    PreReleasePageComponent,
+    JoinPageComponent,
+    ContactPageComponent,
+    InscribePageComponent
   ],
   imports: [
     BrowserModule,
--- starfield/src/app/pages/pages.module.ts
+++ starfield/src/app/pages/pages.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesDirective } from '../pages.directive';
-
 @NgModule({
   declarations: [
     PagesDirective,
